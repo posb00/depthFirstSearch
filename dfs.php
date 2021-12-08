4e728cd6646b0f109c979cbb1c38344d10d1796d
Alexander <posb00@gmail.com>
2021-12-08 14:50:02 -0400

    first commit

---
+++ dfs.php
@@ -0,0 +1,25 @@
+<?php
+
+
+function DepthFirstSearch($root)
+{
+    $stack = [ $root ];
+    $result = [];
+
+    while(count($stack) > 0){
+      
+        $current = array_pop($stack);
+        array_push($result, $current->val);
+        if($current->rigth) array_push($stack, $current->rigth);
+        if($current->left) array_push($stack, $current->left);
+        
+    }
+
+    return $result;
+}
+
+
+
+
+
+?>
